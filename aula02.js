9893bd4394dadb12c9ec33b904f8f484381467c8
Phelipe Schmidt <phelipe.schmidt@icloud.com>
2025-03-25 11:51:36 -0300

    Add files via upload

---
+++ aula02.js
@@ -0,0 +1,13 @@
+/*
+Este eh um arquivo esterno do js
+*/
+
+document.write('Ola Pessoal!'+ "<br>")
+alert('Ola, tudo bem?')
+
+let mensagem = "Este é meu primeiro js"
+let idade = 30
+let altura = 1.9
+document.write(mensagem + "<br>")
+document.write("Idade:" + idade + "<br>")
+document.write("Altura:" + altura + "<br>")
