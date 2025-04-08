364250142a39b571445671a33feb06a9a80c732d
Phelipe Schmidt <phelipe.schmidt@icloud.com>
2025-04-08 10:35:41 -0300

    Add files via upload

---
+++ Aula_04/aula04.js
@@ -0,0 +1,19 @@
+/*
+var frutas = ["maça", "uva"];
+frutas.push("banana");
+frutas.shift();
+frutas.unshift("Abacaxi");
+
+frutas.forEach(function (item, index, array){
+    console.log(index,item)
+})
+
+frutas.splice(1,1);
+console.log(frutas);
+*/
+
+let uf = ["RJ", "SP", "MG"];
+
+console.log(uf[0]); // Vai imprimir RJ
+console.log(uf[1]); // Vai imprimir SP
+console.log(uf[2]); // Vai imprimir MG
